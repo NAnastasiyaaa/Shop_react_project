7128f818750bef16f734579edafbc27e433364ea
Andrii Zhabynets <a.zhabynets03@gmail.com>
2022-02-06 02:43:27 +0200

    items commit

--- shop_react/src/App.js
+++ shop_react/src/App.js
@@ -3,14 +3,14 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Catalog from "./CatalogPage/Catalog";
+import Footer from "./components/ToDo/footer";
 
 function App() {
   return (
     <BrowserRouter>
-      
       <Routes>
         <Route path='/home' element={<><Navbar /><Home/></>}/>
-        <Route path='/catalog' element={<><Navbar /><Catalog/></>}/>
+        <Route path='/catalog' element={<><Navbar /><Catalog/><Footer /></>}/>
       </Routes>
     </BrowserRouter>
   );
--- shop_react/src/CatalogPage/Catalog.js
+++ shop_react/src/CatalogPage/Catalog.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Items from "./Items";
+
 
 function Catalog() {
   return <div>
-      Catalog
+      <Items />
   </div>;
 }
 
